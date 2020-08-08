061a46bfa3eab3c43b7e7ed0c9e0b6540bd17397
simon liu <simonlyllyyy@gmail.com>
2020-08-08 18:20:24 +1000

    edit styles

---
+++ src/pages/Homepage.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "../styles/pages/homepage.styles.scss"
+
+//components
+import HeaderIndex from "../components/header/headerIndex"
+import SubHeaders from "../components/introsection/Sub-header"
+import WebinarSection from "../components/webinar-section/webinar-section"
+import IntroVideo from "../components/introVideo/introVideo"
+import Form from "../components/form/registerForm"
+
+function HomePage() {
+  return (
+    <div className="App">
+      <HeaderIndex />
+      <SubHeaders />
+      <WebinarSection />
+      <IntroVideo />
+      <Form />
+    </div>
+  )
+}
+
+export default HomePage
