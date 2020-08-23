007d4f442371db3efb9283de0cfada8b3a38206c
simon <simonlyllyyy@gmail.com>
2020-08-23 13:35:23 +1000

    fhis is the combination of the latest several commits

---
+++ src/test4squash/test4squash.js
@@ -0,0 +1,4 @@
+
+const commitOne = "This is the first commit for test4squash branch"
+const commitTwo = "This is the second commit for test4squash branch"
+const commitThree = "This is the third commit for thest4squash branch"
